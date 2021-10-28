c719083896bd65bc433ebfb0c3b7020863134b5a
Elia Capiluppi <eliacapiluppi02gmail.com>
2021-10-28 11:37:45 +0200

    Aggiungere i file di progetto.

---
+++ Client1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Premi invio per connetterti");
+            Console.ReadLine();
+            var endpoint = new IPEndPoint(IPAddress.Loopback, 8888);
+            Socket socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Connect(endpoint);
+            NetworkStream networkStream = new NetworkStream(socket);
+            Console.WriteLine("Inserisci il messaggio:");
+            string dataToServer = Console.ReadLine();
+            byte[] bytesToServer = System.Text.Encoding.ASCII.GetBytes(dataToServer);
+            networkStream.Write(bytesToServer, 0, bytesToServer.Length);
+            byte[] bytesFromServer = new byte[10000];
+            networkStream.Read(bytesFromServer, 0, bytesFromServer.Length);
+            string dataFromServer = System.Text.Encoding.ASCII.GetString(bytesFromServer);
+            Console.WriteLine(dataFromServer);
+            //Invio dati verso il client
+            networkStream.Close();
+            socket.Close();
+            socket.Dispose();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var endpoint = new IPEndPoint(IPAddress.Loopback, 8888);
+            Socket serverSocket = new Socket(endpoint.AddressFamily,SocketType.Stream,ProtocolType.Tcp);
+            //Binding
+            serverSocket.Bind(endpoint);
+            //Mi metto in ascolto
+            serverSocket.Listen(128);
+            //Accetto una connessione
+            while (true)
+            {
+
+
+                Socket clientSocket = serverSocket.Accept();
+                //Stream di comunicazione
+                NetworkStream networkStream = new NetworkStream(clientSocket);
+                if (networkStream.CanRead)
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            //Ricevo i dati inviati al client
+                            byte[] bytesFormClient = new byte[10000];
+                            networkStream.Read(bytesFormClient, 0, bytesFormClient.Length);
+                            string dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFormClient);
+
+                            //Invio dati verso il client
+                            string dataToClient = "Echo di: " + dataFromClient;
+                            byte[] bytesToClient = System.Text.Encoding.ASCII.GetBytes(dataToClient);
+                            networkStream.Write(bytesToClient, 0, bytesToClient.Length);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.ToString());
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
